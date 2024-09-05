d3c7e7bf587015e265583fddc333943ee3e5a893
DimitarVen <dimitar.venkov@autodesk.com>
2024-09-05 14:43:32 +0300

    add wrap methods back, mark as obsolete

--- src/Libraries/RevitNodes/Elements/InternalUtilities/ElementWrapper.cs
+++ src/Libraries/RevitNodes/Elements/InternalUtilities/ElementWrapper.cs
@@ -1,4 +1,5 @@
-﻿using Autodesk.DesignScript.Runtime;
+﻿using System;
+using Autodesk.DesignScript.Runtime;
 using Autodesk.Revit.DB;
 using Revit.Elements.Views;
 using DB = Autodesk.Revit.DB;
@@ -283,6 +284,458 @@ namespace Revit.Elements
                     return UnknownElement.FromExisting(ele, isRevitOwned);
             }
         }
-    }
 
+        #region Wrap methods
+
+        [Obsolete("Please use ToDsType instead")]
+        public static UnknownElement Wrap(Autodesk.Revit.DB.Element element, bool isRevitOwned)
+        {
+            return UnknownElement.FromExisting(element, isRevitOwned);
+        }
+
+        [Obsolete("Please use ToDsType instead")]
+        public static AbstractFamilyInstance Wrap(Autodesk.Revit.DB.FamilyInstance ele, bool isRevitOwned)
+        {
+            if (AdaptiveComponentInstanceUtils.HasAdaptiveFamilySymbol(ele))
+            {
+                return AdaptiveComponent.FromExisting(ele, isRevitOwned);
+            }
+
+            if (ele.StructuralType != Autodesk.Revit.DB.Structure.StructuralType.NonStructural &&
+                ele.StructuralType != Autodesk.Revit.DB.Structure.StructuralType.Footing)
+            {
+                return StructuralFraming.FromExisting(ele, isRevitOwned);
+            }
+
+            return FamilyInstance.FromExisting(ele, isRevitOwned);
+        }
+
+        [Obsolete("Please use ToDsType instead")]
+        public static Area Wrap(Autodesk.Revit.DB.Area ele, bool isRevitOwned)
+        {
+            return Area.FromExisting(ele, isRevitOwned);
+        }
+
+        [Obsolete("Please use ToDsType instead")]
+        public static Ceiling Wrap(Autodesk.Revit.DB.Ceiling ele, bool isRevitOwned)
+        {
+            return Ceiling.FromExisting(ele, isRevitOwned);
+        }
+
+        [Obsolete("Please use ToDsType instead")]
+        public static CeilingType Wrap(Autodesk.Revit.DB.CeilingType ele, bool isRevitOwned)
+        {
+            return CeilingType.FromExisting(ele, isRevitOwned);
+        }
+
+        [Obsolete("Please use ToDsType instead")]
+        public static DirectShape Wrap(Autodesk.Revit.DB.DirectShape ele, bool isRevitOwned)
+        {
+            return DirectShape.FromExisting(ele, isRevitOwned);
+        }
+
+        [Obsolete("Please use ToDsType instead")]
+        public static DividedPath Wrap(Autodesk.Revit.DB.DividedPath ele, bool isRevitOwned)
+        {
+            return DividedPath.FromExisting(ele, isRevitOwned);
+        }
+
+        [Obsolete("Please use ToDsType instead")]
+        public static ElementType Wrap(Autodesk.Revit.DB.ElementType elementType, bool isRevitOwned)
+        {
+            return ElementType.FromExisting(elementType, isRevitOwned);
+        }
+
+        [Obsolete("Please use ToDsType instead")]
+        public static DividedSurface Wrap(Autodesk.Revit.DB.DividedSurface ele, bool isRevitOwned)
+        {
+            return DividedSurface.FromExisting(ele, isRevitOwned);
+        }
+
+        [Obsolete("Please use ToDsType instead")]
+        public static Family Wrap(Autodesk.Revit.DB.Family ele, bool isRevitOwned)
+        {
+            return Family.FromExisting(ele, isRevitOwned);
+        }
+
+        [Obsolete("Please use ToDsType instead")]
+        public static FamilyType Wrap(Autodesk.Revit.DB.FamilySymbol ele, bool isRevitOwned)
+        {
+            return FamilyType.FromExisting(ele, isRevitOwned);
+        }
+
+        [Obsolete("Please use ToDsType instead")]
+        public static Floor Wrap(Autodesk.Revit.DB.Floor ele, bool isRevitOwned)
+        {
+            return Floor.FromExisting(ele, isRevitOwned);
+        }
+
+        [Obsolete("Please use ToDsType instead")]
+        public static FloorType Wrap(Autodesk.Revit.DB.FloorType ele, bool isRevitOwned)
+        {
+            return FloorType.FromExisting(ele, isRevitOwned);
+        }
+
+        [Obsolete("Please use ToDsType instead")]
+        public static Form Wrap(Autodesk.Revit.DB.Form ele, bool isRevitOwned)
+        {
+            return Form.FromExisting(ele, isRevitOwned);
+        }
+
+        [SupressImportIntoVM]
+        [Obsolete("Please use ToDsType instead")]
+        public static FreeForm Wrap(Autodesk.Revit.DB.FreeFormElement ele, bool isRevitOwned)
+        {
+            return FreeForm.FromExisting(ele, isRevitOwned);
+        }
+
+        [Obsolete("Please use ToDsType instead")]
+        public static Grid Wrap(Autodesk.Revit.DB.Grid ele, bool isRevitOwned)
+        {
+            return Grid.FromExisting(ele, isRevitOwned);
+        }
+
+        [Obsolete("Please use ToDsType instead")]
+        public static Group Wrap(Autodesk.Revit.DB.Group ele, bool isRevitOwned)
+        {
+            return Group.FromExisting(ele, isRevitOwned);
+        }
+
+        [Obsolete("Please use ToDsType instead")]
+        public static Level Wrap(Autodesk.Revit.DB.Level ele, bool isRevitOwned)
+        {
+            return Level.FromExisting(ele, isRevitOwned);
+        }
+
+        [Obsolete("Please use ToDsType instead")]
+        public static ModelCurve Wrap(Autodesk.Revit.DB.ModelCurve ele, bool isRevitOwned)
+        {
+            return ModelCurve.FromExisting(ele, isRevitOwned);
+        }
+
+        [Obsolete("Please use ToDsType instead")]
+        public static CurveByPoints Wrap(Autodesk.Revit.DB.CurveByPoints ele, bool isRevitOwned)
+        {
+            return CurveByPoints.FromExisting(ele, isRevitOwned);
+        }
+
+        [Obsolete("Please use ToDsType instead")]
+        public static ModelText Wrap(Autodesk.Revit.DB.ModelText ele, bool isRevitOwned)
+        {
+            return ModelText.FromExisting(ele, isRevitOwned);
+        }
+
+        [Obsolete("Please use ToDsType instead")]
+        public static ModelTextType Wrap(Autodesk.Revit.DB.ModelTextType ele, bool isRevitOwned)
+        {
+            return ModelTextType.FromExisting(ele, isRevitOwned);
+        }
+
+        [Obsolete("Please use ToDsType instead")]
+        public static ReferencePlane Wrap(Autodesk.Revit.DB.ReferencePlane ele, bool isRevitOwned)
+        {
+            return ReferencePlane.FromExisting(ele, isRevitOwned);
+        }
+
+        [Obsolete("Please use ToDsType instead")]
+        public static ReferencePoint Wrap(Autodesk.Revit.DB.ReferencePoint ele, bool isRevitOwned)
+        {
+            return ReferencePoint.FromExisting(ele, isRevitOwned);
+        }
+
+        [Obsolete("Please use ToDsType instead")]
+        public static SketchPlane Wrap(Autodesk.Revit.DB.SketchPlane ele, bool isRevitOwned)
+        {
+            return SketchPlane.FromExisting(ele, isRevitOwned);
+        }
+
+        [Obsolete("Please use ToDsType instead")]
+        public static Wall Wrap(Autodesk.Revit.DB.Wall ele, bool isRevitOwned)
+        {
+            return Wall.FromExisting(ele, isRevitOwned);
+        }
+
+        [Obsolete("Please use ToDsType instead")]
+        public static WallType Wrap(Autodesk.Revit.DB.WallType ele, bool isRevitOwned)
+        {
+            return WallType.FromExisting(ele, isRevitOwned);
+        }
+
+        [Obsolete("Please use ToDsType instead")]
+        public static View3D Wrap(Autodesk.Revit.DB.View3D view, bool isRevitOwned)
+        {
+            if (!view.IsTemplate)
+            {
+                if (view.IsPerspective)
+                    return PerspectiveView.FromExisting(view, isRevitOwned);
+                else
+                    return AxonometricView.FromExisting(view, isRevitOwned);
+            }
+            else if (view.IsTemplate)
+            {
+                return View3DTemplate.FromExisting(view, isRevitOwned);
+            }
+            return null;
+        }
+
+        [Obsolete("Please use ToDsType instead")]
+        public static Element Wrap(Autodesk.Revit.DB.ViewPlan view, bool isRevitOwned)
+        {
+            switch (view.ViewType)
+            {
+                case ViewType.CeilingPlan:
+                    return CeilingPlanView.FromExisting(view, isRevitOwned);
+                case ViewType.FloorPlan:
+                    return FloorPlanView.FromExisting(view, isRevitOwned);
+                case ViewType.EngineeringPlan:
+                    return StructuralPlanView.FromExisting(view, isRevitOwned);
+                case ViewType.AreaPlan:
+                    return AreaPlanView.FromExisting(view, isRevitOwned);
+                default:
+                    return UnknownElement.FromExisting(view, true);
+            }
+        }
+
+        [Obsolete("Please use ToDsType instead")]
+        public static SectionView Wrap(Autodesk.Revit.DB.ViewSection view, bool isRevitOwned)
+        {
+            return SectionView.FromExisting(view, isRevitOwned);
+        }
+
+        [Obsolete("Please use ToDsType instead")]
+        public static ScheduleView Wrap(Autodesk.Revit.DB.ViewSchedule view, bool isRevitOwned)
+        {
+            return ScheduleView.FromExisting(view, isRevitOwned);
+        }
+
+        [Obsolete("Please use ToDsType instead")]
+        public static Element Wrap(Autodesk.Revit.DB.View view, bool isRevitOwned)
+        {
+            switch (view.ViewType)
+            {
+                case ViewType.Legend:
+                    return Legend.FromExisting(view, isRevitOwned);
+                default:
+                    return UnknownElement.FromExisting(view, true);
+            }
+        }
+
+        [Obsolete("Please use ToDsType instead")]
+        public static Sheet Wrap(Autodesk.Revit.DB.ViewSheet view, bool isRevitOwned)
+        {
+            return Sheet.FromExisting(view, isRevitOwned);
+        }
+
+        [Obsolete("Please use ToDsType instead")]
+        public static DraftingView Wrap(Autodesk.Revit.DB.ViewDrafting view, bool isRevitOwned)
+        {
+            return DraftingView.FromExisting(view, isRevitOwned);
+        }
+
+        [Obsolete("Please use ToDsType instead")]
+        public static Topography Wrap(Autodesk.Revit.DB.Architecture.TopographySurface topoSurface, bool isRevitOwned)
+        {
+            return Topography.FromExisting(topoSurface, isRevitOwned);
+        }
+
+        [Obsolete("Please use ToDsType instead")]
+        public static Toposolid Wrap(Autodesk.Revit.DB.Toposolid toposolid, bool isRevitOwned)
+        {
+            return Toposolid.FromExisting(toposolid, isRevitOwned);
+        }
+
+        [Obsolete("Please use ToDsType instead")]
+        public static ToposolidType Wrap(Autodesk.Revit.DB.ToposolidType toposolidType, bool isRevitOwned)
+        {
+            return ToposolidType.FromExisting(toposolidType, isRevitOwned);
+        }
+
+        [Obsolete("Please use ToDsType instead")]
+        public static object Wrap(Autodesk.Revit.DB.Panel ele, bool isRevitOwned)
+        {
+            if (AdaptiveComponentInstanceUtils.IsAdaptiveFamilySymbol(ele.Symbol))
+            {
+                return AdaptiveComponent.FromExisting(ele, isRevitOwned);
+            }
+
+            return CurtainPanel.FromExisting(ele, isRevitOwned);
+        }
+
+        [Obsolete("Please use ToDsType instead")]
+        public static Mullion Wrap(Autodesk.Revit.DB.Mullion ele, bool isRevitOwned)
+        {
+            return Mullion.FromExisting(ele, isRevitOwned);
+        }
+
+        [Obsolete("Please use ToDsType instead")]
+        public static Dimension Wrap(Autodesk.Revit.DB.Dimension ele, bool isRevitOwned)
+        {
+            return Dimension.FromExisting(ele, isRevitOwned);
+        }
+
+        [Obsolete("Please use ToDsType instead")]
+        public static DimensionType Wrap(Autodesk.Revit.DB.DimensionType ele, bool isRevitOwned)
+        {
+            return DimensionType.FromExisting(ele, isRevitOwned);
+        }
+
+        [Obsolete("Please use ToDsType instead")]
+        public static FilledRegionType Wrap(Autodesk.Revit.DB.FilledRegionType ele, bool isRevitOwned)
+        {
+            return FilledRegionType.FromExisting(ele, isRevitOwned);
+        }
+
+        [Obsolete("Please use ToDsType instead")]
+        public static FilledRegion Wrap(Autodesk.Revit.DB.FilledRegion ele, bool isRevitOwned)
+        {
+            return FilledRegion.FromExisting(ele, isRevitOwned);
+        }
+
+        [Obsolete("Please use ToDsType instead")]
+        public static FillPatternElement Wrap(Autodesk.Revit.DB.FillPatternElement ele, bool isRevitOwned)
+        {
+            return FillPatternElement.FromExisting(ele, isRevitOwned);
+        }
+
+        [Obsolete("Please use ToDsType instead")]
+        public static LinePatternElement Wrap(Autodesk.Revit.DB.LinePatternElement ele, bool isRevitOwned)
+        {
+            return LinePatternElement.FromExisting(ele, isRevitOwned);
+        }
+
+        [Obsolete("Please use ToDsType instead")]
+        public static TextNote Wrap(Autodesk.Revit.DB.TextNote ele, bool isRevitOwned)
+        {
+            return TextNote.FromExisting(ele, isRevitOwned);
+        }
+
+        [Obsolete("Please use ToDsType instead")]
+        public static Tag Wrap(Autodesk.Revit.DB.IndependentTag ele, bool isRevitOwned)
+        {
+            return Tag.FromExisting(ele, isRevitOwned);
+        }
+
+        [Obsolete("Please use ToDsType instead")]
+        public static TextNoteType Wrap(Autodesk.Revit.DB.TextNoteType ele, bool isRevitOwned)
+        {
+            return TextNoteType.FromExisting(ele, isRevitOwned);
+        }
+
+        [Obsolete("Please use ToDsType instead")]
+        public static Revision Wrap(Autodesk.Revit.DB.Revision ele, bool isRevitOwned)
+        {
+            return Revision.FromExisting(ele, isRevitOwned);
+        }
+
+        [Obsolete("Please use ToDsType instead")]
+        public static RevisionCloud Wrap(Autodesk.Revit.DB.RevisionCloud ele, bool isRevitOwned)
+        {
+            return RevisionCloud.FromExisting(ele, isRevitOwned);
+        }
+
+        [Obsolete("Please use ToDsType instead")]
+        public static Revit.Filter.ParameterFilterElement Wrap(Autodesk.Revit.DB.ParameterFilterElement ele, bool isRevitOwned)
+        {
+            return Revit.Filter.ParameterFilterElement.FromExisting(ele, isRevitOwned);
+        }
+
+        [Obsolete("Please use ToDsType instead")]
+        public static Room Wrap(Autodesk.Revit.DB.Architecture.Room ele, bool isRevitOwned)
+        {
+            return Room.FromExisting(ele, isRevitOwned);
+        }
+
+        [Obsolete("Please use ToDsType instead")]
+        public static DetailCurve Wrap(Autodesk.Revit.DB.DetailCurve ele, bool isRevitOwned)
+        {
+            return DetailCurve.FromExisting(ele, isRevitOwned);
+
+        }
+
+        [Obsolete("Please use ToDsType instead")]
+        public static ImportInstance Wrap(Autodesk.Revit.DB.ImportInstance ele, bool isRevitOwned)
+        {
+            return ImportInstance.FromExisting(ele, isRevitOwned);
+        }
+
+        [Obsolete("Please use ToDsType instead")]
+        public static GlobalParameter Wrap(Autodesk.Revit.DB.GlobalParameter ele, bool isRevitOwned)
+        {
+            return GlobalParameter.FromExisting(ele, isRevitOwned);
+        }
+
+        [Obsolete("Please use ToDsType instead")]
+        public static FaceWall Wrap(Autodesk.Revit.DB.FaceWall ele, bool isRevitOwned)
+        {
+            return FaceWall.FromExisting(ele, isRevitOwned);
+        }
+
+        [Obsolete("Please use ToDsType instead")]
+        public static CurtainSystem Wrap(Autodesk.Revit.DB.CurtainSystem ele, bool isRevitOwned)
+        {
+            return CurtainSystem.FromExisting(ele, isRevitOwned);
+        }
+
+        [Obsolete("Please use ToDsType instead")]
+        public static Material Wrap(Autodesk.Revit.DB.Material ele, bool isRevitOwned)
+        {
+            return Material.FromExisting(ele, isRevitOwned);
+        }
+
+        [Obsolete("Please use ToDsType instead")]
+        public static PathOfTravel Wrap(Autodesk.Revit.DB.Analysis.PathOfTravel ele, bool isRevitOwned)
+        {
+            return PathOfTravel.FromExisting(ele, isRevitOwned);
+        }
+
+        [Obsolete("Please use ToDsType instead")]
+        public static Viewport Wrap(Autodesk.Revit.DB.Viewport ele, bool isRevitOwned)
+        {
+            return Viewport.FromExisting(ele, isRevitOwned);
+        }
+
+        [Obsolete("Please use ToDsType instead")]
+        public static ElevationMarker Wrap(Autodesk.Revit.DB.ElevationMarker ele, bool isRevitOwned)
+        {
+            return ElevationMarker.FromExisting(ele, isRevitOwned);
+        }
+
+        [Obsolete("Please use ToDsType instead")]
+        public static Space Wrap(Autodesk.Revit.DB.Mechanical.Space ele, bool isRevitOwned)
+        {
+            return Space.FromExisting(ele, isRevitOwned);
+        }
+
+        [Obsolete("Please use ToDsType instead")]
+        public static RoofType Wrap(Autodesk.Revit.DB.RoofType ele, bool isRevitOwned)
+        {
+            return RoofType.FromExisting(ele, isRevitOwned);
+        }
+
+        [Obsolete("Please use ToDsType instead")]
+        public static Roof Wrap(Autodesk.Revit.DB.RoofBase ele, bool isRevitOwned)
+        {
+            return Roof.FromExisting(ele, isRevitOwned);
+        }
+
+        [Obsolete("Please use ToDsType instead")]
+        public static ScheduleOnSheet Wrap(Autodesk.Revit.DB.ScheduleSheetInstance ele, bool isRevitOwned)
+        {
+            return ScheduleOnSheet.FromExisting(ele, isRevitOwned);
+        }
+
+        [Obsolete("Please use ToDsType instead")]
+        public static AppearanceAssetElement Wrap(Autodesk.Revit.DB.AppearanceAssetElement ele, bool isRevitOwned)
+        {
+            return AppearanceAssetElement.FromExisting(ele, isRevitOwned);
+        }
+
+        [Obsolete("Please use ToDsType instead")]
+        public static LinkInstance Wrap(Autodesk.Revit.DB.RevitLinkInstance ele, bool isRevitOwned)
+        {
+            return LinkInstance.FromExisting(ele, isRevitOwned);
+        }
+
+        #endregion
+    }
 }
